10fc314bbb181cff05b128af796e00e340850580
Brandon Mikelaitis <brandonmikelaitis@Brandons-MBP.localdomain>
2023-12-26 15:13:51 -0700

    add everything

---
+++ app/api/customervideos/route.ts
@@ -0,0 +1,31 @@
+import { client } from '../../contentful/util';
+
+// Retrieve the list of Customer Photos from Contentful
+export async function GET() {
+    try {
+        const response = await client.getEntries({
+          'fields.show': true,
+          content_type: 'customerVideos',
+          limit: 50,
+          order: ['-fields.orderId'] //-to descend
+        });
+          
+        return new Response(JSON.stringify( response.items ), {
+            headers: {
+              'Content-Type': 'application/json',
+              'Cache-Control': 's-maxage=300, stale-while-revalidate'
+            },
+            status: 200,
+            statusText: 'OK'
+          });
+    } catch (error) {
+        console.error(error)
+        return new Response(JSON.stringify({error: `${error}`}), {
+          status: 500,
+          statusText: 'Internal Server Error'
+        })
+      }
+};
+
+  
+      
---
+++ app/contentful/util.ts
@@ -0,0 +1,7 @@
+import { createClient } from 'contentful';
+
+export const client = createClient({
+    space: 'feujzg2f0w3f',
+    accessToken: 'nylenW5zO1E0DqA11tXBjqSJZwXZIxIJFTW93VRNHO8',
+});
+
